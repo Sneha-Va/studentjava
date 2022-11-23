f77703487a0db25dbeeb8e00e0e9f2de73cc4f06
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-23 14:33:22 +0530

    added

--- Student.java
+++ Student.java
@@ -1,3 +1,6 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
 public class Student {
@@ -22,6 +25,30 @@ public class Student {
             switch (choice) {
                 case 1:
                     System.out.println("Add student");
+                    System.out.println("Enter name: ");
+                    name = input.next();
+                    System.out.println("Enter the rollnumber: ");
+                    rollno = input.nextInt();
+                    System.out.println("Enter the admission number: ");
+                    admnum = input.nextInt();
+                    System.out.println("Enter the college: ");
+                    college = input.next();
+
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb","root","");
+                        String sql = "INSERT INTO `students`(`name`, `rollnumber`, `admnumber`, `college`) VALUES (?,?,?,?)";
+                        PreparedStatement stmt = con.prepareStatement(sql);
+                        stmt.setString(1,name);
+                        stmt.setInt(2,rollno);
+                        stmt.setInt(3,admnum);
+                        stmt.setString(4,college);
+                        stmt.executeUpdate();
+                    }
+                    catch (Exception e ){
+                        System.out.println(e);
+                    }
+
                 case 2:
                     System.out.println("View all student");
                 case 3:
